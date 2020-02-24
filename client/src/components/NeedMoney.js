958b85e241f41793a66f045a06960dbc85ee57d4
niazox <grigory.b@hyper-dev.ru>
2020-02-24 21:35:06 +0300

    need money

---
+++ client/src/components/NeedMoney.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NeedMoney extends React.Component {
+    render () {
+        return (
+        <div className="money-page">
+            <p className="money-page__text">заплатите разрабу чеканной монетой</p>
+        </div>
+        )
+    }
+}
